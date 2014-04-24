2cc0d64373d7b6d40b26e39ec3dde73588f8b009
Matthew Elphick <mae.me.uk@gmail.com>
2014-04-24 03:33:32 +0100

    Fix RoomAPI, now named RoomAPIController
    But still referred to by the url api/RoomAPI/{building code}, because
    that's how C# MVC works apparently.

--- TeamProjects/Controllers/api/RoomAPIController.cs
+++ TeamProjects/Controllers/api/RoomAPIController.cs
@@ -12,17 +12,17 @@ using TeamProjects.Models;
 
 namespace TeamProjects.Controllers.api
 {
-    public class RoomAPI : ApiController
+    public class RoomAPIController : ApiController
     {
         private team04Entities db = new team04Entities();
 
-        // GET api/RoomAPI
+        // GET api/RoomAPIController
         public IEnumerable<timetable_room> Gettimetable_room()
         {
             return db.timetable_room.AsEnumerable();
         }
 
-        // GET api/RoomAPI/5
+        // GET api/RoomAPIController/5
         public IEnumerable<timetable_room> Gettimetable_room(string BuildingID)
         {
             IEnumerable<timetable_room> timetable_room = db.timetable_room.Where(bc => bc.Building_ID == BuildingID);
