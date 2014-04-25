6f1558d89c3f5cd009e9ef96a26f0fb86c9f59cf
Matthew Elphick <mae.me.uk@gmail.com>
2014-04-25 18:08:47 +0100

    Add basics for requests (current/previous)

---
+++ TeamProjects/Controllers/RequestsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TeamProjects.Models;
+
+namespace TeamProjects.Controllers
+{
+    public class RequestsController : Controller
+    {
+        private team04Entities db = new team04Entities();
+
+        //
+        // GET: /Requests/
+
+        public ActionResult Index()
+        {
+            return View(db.timetable_request.ToList());
+        }
+
+        //
+        // GET: /Requests/Details/5
+
+        public ActionResult Details(short id = 0)
+        {
+            timetable_request timetable_request = db.timetable_request.Find(id);
+            if (timetable_request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timetable_request);
+        }
+
+        //
+        // GET: /Requests/Create
+
+        public void Create()
+        {
+            Response.Redirect("~/Request/Create");
+        }
+
+        //
+        // POST: /Requests/Create
+
+        [HttpPost]
+        public void Create(timetable_request timetable_request)
+        {
+            Response.Redirect("~/Request/Create");
+        }
+
+        //
+        // GET: /Requests/Edit/5
+
+        public ActionResult Edit(short id = 0)
+        {
+            timetable_request timetable_request = db.timetable_request.Find(id);
+            if (timetable_request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timetable_request);
+        }
+
+        //
+        // POST: /Requests/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(timetable_request timetable_request)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(timetable_request).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(timetable_request);
+        }
+
+        //
+        // GET: /Requests/Delete/5
+
+        public ActionResult Delete(short id = 0)
+        {
+            timetable_request timetable_request = db.timetable_request.Find(id);
+            if (timetable_request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(timetable_request);
+        }
+
+        //
+        // POST: /Requests/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            timetable_request timetable_request = db.timetable_request.Find(id);
+            db.timetable_request.Remove(timetable_request);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
