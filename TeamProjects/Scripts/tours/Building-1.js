a846247ed1d557eba5f47151c62716204a6598f5
Matthew Elphick <mae.me.uk@gmail.com>
2014-04-25 15:31:05 +0100

    Add Hemesh's work

---
+++ TeamProjects/Scripts/tours/Building-1.js
@@ -0,0 +1,22 @@
+﻿var tour;
+
+tour = new Shepherd.Tour({
+    defaults: {
+        classes: 'shepherd-element shepherd-open shepherd-theme-arrows',
+        scrollTo: true
+    }
+});
+
+tour.addStep('add-request-start', {
+    title: 'Create a new building',
+    text: 'TEST',
+    attachTo: '.helloworld',
+    classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
+    buttons: [
+      {
+          text: 'Next',
+          action: tour.next,
+          classes: 'shepherd-button-example-primary'
+      }
+    ]
+});
