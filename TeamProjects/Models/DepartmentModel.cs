f6dbd442c002e5508d59d37022866ed272da0e64
Matthew Elphick <mae.me.uk@gmail.com>
2014-04-29 19:12:49 +0100

    Add Department Model for login system

---
+++ TeamProjects/Models/DepartmentModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TeamProjects.Models
+{
+    public class DepartmentModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(4)]
+        public string Department { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(200)]
+        public string Department { get; set; }
+    }
+}
