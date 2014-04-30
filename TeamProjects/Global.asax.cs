60454820f088601ff35a304762e6c0acc201dd28
Niall <niall@live.co.uk>
2014-04-30 18:20:45 +0100

    Added ignoring circular reference

--- TeamProjects/Global.asax.cs
+++ TeamProjects/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Newtonsoft.Json;
 
 namespace TeamProjects
 {
@@ -25,6 +26,12 @@ namespace TeamProjects
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            };
         }
     }
 }
