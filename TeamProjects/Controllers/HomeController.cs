280be52922917c50daf9d741262173c88943c659
Matthew Elphick <mae.me.uk@gmail.com>
2014-05-08 16:40:34 +0100

    Add basic information to index page

--- TeamProjects/Controllers/HomeController.cs
+++ TeamProjects/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeamProjects.Models;
 
 namespace TeamProjects.Controllers
 {
@@ -11,6 +12,11 @@ namespace TeamProjects.Controllers
         [Authorize]
         public ActionResult Index()
         {
+            team04Entities db = new team04Entities();
+            int toSkip = (db.timetable_request.Count() > 5) ? db.timetable_request.Count() - 5 : 0;
+            ViewBag.RecentlyAdded = db.timetable_request.OrderBy(r => r.Request_ID).Skip(toSkip).OrderByDescending(r => r.Request_ID);
+            ViewBag.Attention = db.timetable_request.Where(r => r.Request_Status == 3 || r.Request_Status == 4).OrderByDescending(r => r.Request_ID);
+            ViewBag.RoundInfo = db.timetable_round.Where(r => r.Round_Status == "Current").FirstOrDefault();
             return View();
         }
     }
