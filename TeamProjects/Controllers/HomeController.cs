4dc2e39708011fb1501dbeebdfd49465951eb419
Matthew Elphick <mae.me.uk@gmail.com>
2014-05-08 16:47:50 +0100

    Change index so table length calculated only once

--- TeamProjects/Controllers/HomeController.cs
+++ TeamProjects/Controllers/HomeController.cs
@@ -13,7 +13,8 @@ namespace TeamProjects.Controllers
         public ActionResult Index()
         {
             team04Entities db = new team04Entities();
-            int toSkip = (db.timetable_request.Count() > 5) ? db.timetable_request.Count() - 5 : 0;
+            int requestCount = db.timetable_request.Count();
+            int toSkip = (requestCount > 5) ? requestCount - 5 : 0;
             ViewBag.RecentlyAdded = db.timetable_request.OrderBy(r => r.Request_ID).Skip(toSkip).OrderByDescending(r => r.Request_ID);
             ViewBag.Attention = db.timetable_request.Where(r => r.Request_Status == 3 || r.Request_Status == 4).OrderByDescending(r => r.Request_ID);
             ViewBag.RoundInfo = db.timetable_round.Where(r => r.Round_Status == "Current").FirstOrDefault();
