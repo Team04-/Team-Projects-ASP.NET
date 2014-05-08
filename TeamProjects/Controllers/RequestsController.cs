ff305575d1defd7bfb9154b776ab5db692a8848e
Matthew Elphick <mae.me.uk@gmail.com>
2014-05-08 19:51:13 +0100

    Prepare to remove old list view and edit changes

--- TeamProjects/Controllers/RequestsController.cs
+++ TeamProjects/Controllers/RequestsController.cs
@@ -12,134 +12,104 @@ namespace TeamProjects.Controllers
     public class RequestsController : Controller
     {
         private team04Entities db = new team04Entities();
-
-        //
-        // GET: /Requests/
-        [Authorize]
-        public ActionResult Index()
+        private Models.RequestListModel getFullRequest(Models.timetable_request request)
         {
-            var check = db.timetable_round.Where(r => r.Round_Status == "Current").First();
-            if (!(check is timetable_round))
+            Models.RequestListModel newRequest = new Models.RequestListModel();
+            newRequest.Request_ID = request.Request_ID;
+            newRequest.Module_Code = request.Module_Code;
+            newRequest.Custom_Comments = request.Custom_Comments;
+            newRequest.Round = request.Current_Round;
+            newRequest.Park = request.Park_ID;
+            newRequest.Priority = request.Priority;
+            newRequest.Number_Rooms = request.Number_Rooms;
+            newRequest.Number_Students = request.Number_Students;
+            newRequest.Start_Period = request.Start_Time;
+            string weekDay = "";
+            switch (int.Parse(request.Day_ID.ToString()))
             {
-                check = db.timetable_round.Last();
+                case 1:
+                    weekDay = "Mon";
+                    break;
+                case 2:
+                    weekDay = "Tues";
+                    break;
+                case 3:
+                    weekDay = "Wed";
+                    break;
+                case 4:
+                    weekDay = "Thur";
+                    break;
+                case 5:
+                    weekDay = "Fri";
+                    break;
+                case 6:
+                    weekDay = "Sat";
+                    break;
+                case 7:
+                    weekDay = "Sun";
+                    break;
+                default:
+                    weekDay = "Unknown";
+                    break;
             }
-            ViewBag.currentRoundCode = check.Round_Code;
-            return View(db.timetable_request.OrderBy(r => r.Current_Round).ToList());
-        }
-        
-        //
-        //GET: /Requests/List
-        [Authorize]
-        public ActionResult List()
-        {
-            List<Models.RequestListModel> RequestList = new List<Models.RequestListModel>();
-            List<timetable_request> Requests = db.timetable_request.OrderBy(r => r.Current_Round).ToList();
-            foreach(timetable_request request in Requests)
+            newRequest.Day = weekDay;
+            string status = "";
+            switch (int.Parse(request.Request_Status.ToString()))
             {
-                Models.RequestListModel newRequest = new Models.RequestListModel();
-                newRequest.Request_ID = request.Request_ID;
-                newRequest.Module_Code = request.Module_Code;
-                newRequest.Custom_Comments = request.Custom_Comments;
-                newRequest.Round = request.Current_Round;
-                newRequest.Park = request.Park_ID;
-                newRequest.Priority = request.Priority;
-                newRequest.Number_Rooms = request.Number_Rooms;
-                newRequest.Number_Students = request.Number_Students;
-                newRequest.Start_Period = request.Start_Time;
-                string weekDay = "";
-                switch (int.Parse(request.Day_ID.ToString()))
-                {
-                    case 1:
-                        weekDay = "Mon";
-                        break;
-                    case 2:
-                        weekDay = "Tues";
-                        break;
-                    case 3:
-                        weekDay = "Wed";
-                        break;
-                    case 4:
-                        weekDay = "Thur";
-                        break;
-                    case 5:
-                        weekDay = "Fri";
-                        break;
-                    case 6:
-                        weekDay = "Sat";
-                        break;
-                    case 7:
-                        weekDay = "Sun";
-                        break;
-                    default:
-                        weekDay = "Unknown";
-                        break;
-                }
-                newRequest.Day = weekDay;
-                string status = "";
-                switch (int.Parse(request.Request_Status.ToString()))
-                {
-                    case 1:
-                        status = "Pending";
-                        break;
-                    case 2:
-                        status = "Accepted";
-                        break;
-                    case 3:
-                        status = "Failed";
-                        break;
-                    case 4:
-                        status = "Altered";
-                        break;
-                    default:
-                        status = "Unknown";
-                        break;
-                }
-                newRequest.Status = status;
-
-                newRequest.Module_Title = db.timetable_module.Where(m => m.Module_Code == request.Module_Code).First().Module_Title;
-                newRequest.Has_Comments = (request.Custom_Comments == null) ? (false) : (!(request.Custom_Comments.Trim() == ""));
-                newRequest.End_Period = ((request.Start_Time + request.Duration)-1);
-
-                newRequest.Time_String = GetTimeString(newRequest.Start_Period) + " - " + GetTimeString(newRequest.End_Period+1);
-
-                List<timetable_request_week> weekList = db.timetable_request_week.Where(rw => rw.Request_ID == request.Request_ID).ToList();
-                int[] weekListArray = new int[weekList.Count];
-                int weekCounter = 0;
-                foreach (timetable_request_week week in weekList)
-                {
-                    weekListArray[weekCounter] = week.Week;
-                }
-                bool[] weekArray = new bool[15];
-                for (var i = 0; i < 15; i++)
-                {
-                    if (weekListArray.Contains(i + 1))
-                    {
-                        weekArray[i] = true;
-                    }
-                    else
-                    {
-                        weekArray[i] = false;
-                    }
-                }
-                newRequest.Weeks = weekArray;
-                List<timetable_request_room_allocation> roomList = db.timetable_request_room_allocation.Where(ra => ra.Request_ID == request.Request_ID).ToList();
-                string[] roomArray = new string[roomList.Count];
-                int roomCounter = 0;
-                foreach(timetable_request_room_allocation room in roomList)
-                {
-                    roomArray[roomCounter] = room.Building_ID.ToString() + room.Room_ID.ToString();
-                    roomCounter++;
-                }
-                newRequest.Rooms = roomArray;
-                RequestList.Add(newRequest);
+                case 1:
+                    status = "Pending";
+                    break;
+                case 2:
+                    status = "Accepted";
+                    break;
+                case 3:
+                    status = "Failed";
+                    break;
+                case 4:
+                    status = "Altered";
+                    break;
+                default:
+                    status = "Unknown";
+                    break;
             }
-            var check = db.timetable_round.Where(r => r.Round_Status == "Current").First();
-            if (!(check is timetable_round))
+            newRequest.Status = status;
+
+            newRequest.Module_Title = db.timetable_module.Where(m => m.Module_Code == request.Module_Code).First().Module_Title;
+            newRequest.Has_Comments = (request.Custom_Comments == null) ? (false) : (!(request.Custom_Comments.Trim() == ""));
+            newRequest.End_Period = ((request.Start_Time + request.Duration) - 1);
+
+            newRequest.Time_String = GetTimeString(newRequest.Start_Period) + " - " + GetTimeString(newRequest.End_Period + 1);
+
+            List<timetable_request_week> weekList = db.timetable_request_week.Where(rw => rw.Request_ID == request.Request_ID).ToList();
+            int[] weekListArray = new int[weekList.Count];
+            int weekCounter = 0;
+            foreach (timetable_request_week week in weekList)
             {
-                check = db.timetable_round.Last();
+                weekListArray[weekCounter] = week.Week;
             }
-            ViewBag.currentRoundCode = check.Round_Code;
-            return View(RequestList.OrderBy(r => r.Request_ID).ThenByDescending(r => r.Round).ToList());
+            bool[] weekArray = new bool[15];
+            for (var i = 0; i < 15; i++)
+            {
+                if (weekListArray.Contains(i + 1))
+                {
+                    weekArray[i] = true;
+                }
+                else
+                {
+                    weekArray[i] = false;
+                }
+            }
+            newRequest.Weeks = weekArray;
+            List<timetable_request_room_allocation> roomList = db.timetable_request_room_allocation.Where(ra => ra.Request_ID == request.Request_ID).ToList();
+            string[] roomArray = new string[roomList.Count];
+            int roomCounter = 0;
+            foreach (timetable_request_room_allocation room in roomList)
+            {
+                roomArray[roomCounter] = room.Building_ID.ToString() + room.Room_ID.ToString();
+                roomCounter++;
+            }
+            newRequest.Rooms = roomArray;
+            return newRequest;
         }
         private string GetTimeString(int period)
         {
@@ -179,6 +149,39 @@ namespace TeamProjects.Controllers
             }
             return time;
         }
+        //
+        // GET: /Requests/
+        [Authorize]
+        public ActionResult Index()
+        {
+            var check = db.timetable_round.Where(r => r.Round_Status == "Current").First();
+            if (!(check is timetable_round))
+            {
+                check = db.timetable_round.Last();
+            }
+            ViewBag.currentRoundCode = check.Round_Code;
+            return View(db.timetable_request.OrderBy(r => r.Current_Round).ToList());
+        }
+        
+        //
+        //GET: /Requests/List
+        [Authorize]
+        public ActionResult List()
+        {
+            List<Models.RequestListModel> RequestList = new List<Models.RequestListModel>();
+            List<timetable_request> Requests = db.timetable_request.OrderBy(r => r.Current_Round).ToList();
+            foreach(timetable_request request in Requests)
+            {
+                RequestList.Add(getFullRequest(request));
+            }
+            var check = db.timetable_round.Where(r => r.Round_Status == "Current").First();
+            if (!(check is timetable_round))
+            {
+                check = db.timetable_round.Last();
+            }
+            ViewBag.currentRoundCode = check.Round_Code;
+            return View(RequestList.OrderBy(r => r.Request_ID).ThenByDescending(r => r.Round).ToList());
+        }
 
         //
         // GET: /Requests/Details/5
@@ -212,14 +215,14 @@ namespace TeamProjects.Controllers
         //
         // GET: /Requests/Edit/5
         [Authorize]
-        public ActionResult Edit(short id = 0)
+        public ActionResult Edit(short id)
         {
-            timetable_request timetable_request = db.timetable_request.Find(id);
-            if (timetable_request == null)
+            timetable_request request = db.timetable_request.Where(r => r.Request_ID == id).FirstOrDefault();
+            if (id == null)
             {
                 return HttpNotFound();
             }
-            return View(timetable_request);
+            return View(getFullRequest(request));
         }
 
         //
